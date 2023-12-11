223cc1e0d9514ffc962d8de6380ef0386025a1ec
Caljorb <r.caleb.jones@icloud.com>
2023-12-10 20:55:46 -0500

    Made screen appear

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Pong
 First attempt at a project in Python. Trying to make Pong that runs in a terminal.
+
+To start virtual env, run 
+    source pongenv/bin/activate
---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+
+# screen
+#turtle.setup(600, 500)
+# screen color
+#turtle.bgcolor("black")
+
+#turtle.forward(120)
+screen = turtle.Screen()
+screen.setup(600, 500)
+screen.bgcolor("black")
+screen.exitonclick()
+
+
