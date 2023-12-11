20846c18d5b7c6a2c88336d9568c2c5f002dc997
Caljorb <r.caleb.jones@icloud.com>
2023-12-11 17:39:33 -0500

    Added start text to beginning screen

--- main.py
+++ main.py
@@ -13,7 +13,24 @@ screen.bgcolor("black")
 screen.addshape("./res/colon-three-kitty.gif")
 
 # display on screen
-t.shape("./res/colon-three-kitty.gif")
+cat = t.Turtle()
+cat.shape("./res/colon-three-kitty.gif")
+
+# move turtle
+start = t.Turtle()
+start.hideturtle() # arrow isn't visible
+start.up() # don't draw while moving
+start.left(90)
+start.forward(screen.window_height() / 2 - 100)
+start.right(90)
+start.forward(screen.window_width() / 4)
+
+# write start
+f = ('comic sans', 25)
+start.write("Start", font=f, align="Center")
+
+
+
 
 # Close screen
 screen.exitonclick()
